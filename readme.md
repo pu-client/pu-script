55123f12abac819e784a69b198632a0555d1f758
seiuna <nmmlwc@gmail.com>
2024-04-20 19:16:09 +0800

    first

---
+++ readme.md
@@ -0,0 +1,8 @@
+# 介绍
+这是一个在浏览器上运行的js脚本，用于快速的获得pu口袋校园中的活动。
+# 使用方法
+1. 打开pu口袋校园，进入活动页面 https://pc.pocketuni.net/active/detail?id=xxxxx 。
+2. 打开开发者工具后点击console/控制台复制script.js中的脚本到控制台中运行。
+   ![好好好](./img/1.png)
+3. 之后等待脚本运行即可。
+   ![好好好](./img/2.png)
---
+++ script.js
@@ -0,0 +1,28 @@
+const target=(()=> {
+        const c=document.getElementsByClassName("cursor-pointer")
+        for (let i = 0; i < c.length; i++) {
+            const ele=c[i];
+            if(ele.innerHTML.includes("我要报名")){
+                console.log("已找到目标元素!");
+                ele.disabled=false;
+                return ele;
+            }
+        }
+    }
+)();
+const start=(()=>{
+    const d=document.getElementsByClassName("text-[14px] text-[#666]")[0]
+    const t=document.getElementsByClassName("text-[24px] text-[#434447] font-bold")[0]
+    const time=new Date(d.innerHTML+t.innerHTML);
+    console.log("已找到目标时间: "+time)
+    return time.getTime();
+})()
+
+setInterval(()=>{
+    const now=new Date().getTime();
+    if(now>=start){
+        console.log("时间到，开始点击!")
+        target.click();
+    }
+    console.log("剩余时间: "+Math.floor((start-now)/1000)+"s")
+},110)
